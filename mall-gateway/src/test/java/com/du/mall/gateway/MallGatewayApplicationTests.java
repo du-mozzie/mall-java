38aa4ff96e80e274282e608fe446c2bd33c85e05
Du <2548425238@qq.com>
2022-06-04 14:38:08 +0800

    gateway

---
+++ mall-gateway/src/test/java/com/du/mall/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
